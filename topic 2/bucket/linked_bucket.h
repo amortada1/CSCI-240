c12708082b43b5c7c69d5a6df61f7423781f2f34
amortada1 <alimortada05@gmail.com>
2024-03-18 10:32:36 -0700

    updated linked_bucket

--- topic 2/bucket/linked_bucket.h
+++ topic 2/bucket/linked_bucket.h
@@ -10,26 +10,32 @@ class LinkedBucket : public Bucket<T>
 {
 public:
     // constructor
-    LinkedBucket() : head{nullptr} {}
+    LinkedBucket() : count{}, head{nullptr} {}
 
     // destructor
-    ~LinkedBucket() {while (!empty()) remove();}
+    ~LinkedBucket() {clear();}
 
     // adds an item to the bucket
     void add(const T& item) override
     {
         head = new Node{item, head};
+        ++count;
     }
 
     // removes and returns an arbitrary item from the bucket
     T remove() override  
     {
-        if(empty()) throw std::logic_error("remove on an empty bucket! >:(");
+        T rtnItem;
 
-        Node* tmp{head};
-        T rtnItem = head->item;  // create temporary variable to return item's value after deleting
-        head = head->next;
-        delete tmp;
+        // Check if bucket is empty first, then continue with deletion
+        if(empty()) throw std::logic_error("remove on an empty bucket! >:(");
+        
+        rtnItem = head->item;  // create temporary variable to return item's value after deleting
+        
+        removeHead();
+
+        --count;
+
         return rtnItem;  
     }
 
@@ -39,23 +45,27 @@ public:
         bool removed{};
         Node* current{head};
 
-        // iterate thru linked list and compare items
         while (current != nullptr)
         {
-            if (current->item == item)  // if current node's item matches argument, remove it
+            if (current->item == item) 
             {
-                Node* tmp{current};
-                current = current->next;
-                delete tmp;
                 removed = true;
+                --count;
+
+                if (current == head)
+                {
+                    current = current->next;
+                    removeHead();
+                    continue;
+                }
+
+                current->item = head->item;
+                removeHead();
             }
-            else
-            {
-                current = current->next;  // otherwise, traverse to the next node
-            }
+
+            current = current->next;
         }
 
-        delete current;
         return removed;
     }
 
@@ -69,45 +79,36 @@ public:
             current = current->next;
         }
 
-        delete current;
         return false;
     }
 
     // returns an array of items
     T* toArray() const override 
     {
-        size_t num{count()};
         Node* current{head};
-        T* rtnArray{new T[num]};
-        
-        for (size_t i = 0; i < num; ++i, current = current->next)
+        T* rtnArray{new T[count]};
+
+        for (size_t i = 0; i < count; ++i)
         {
             rtnArray[i] == current->item;
+            current = current->next;
         }
 
-        delete current;
         return rtnArray;
     }
 
     // empties the collection
     void clear() override 
     {
-        Node* current{head};
-        while (current->next != nullptr)
-        {
-            Node* tmp{current};
-            current = current->next;
-            delete tmp;
-        }
-
-        delete current;
+        while (head != nullptr) {removeHead();}
+        count = 0;
     }
 
     // returns the number of items in the collection
-    size_t size() const override {return count();}
+    size_t size() const override {return count;}
 
     // returns true if empty and false otherwise
-    bool empty() const override {return head == nullptr;}
+    bool empty() const override {return count == 0;}
 
 private:
     struct Node
@@ -117,21 +118,16 @@ private:
     };
 
     Node* head;
+    size_t count;
 
-    // Helper function to find the size of linked list
-    size_t count() const
+    // Helper function to remove head
+    void removeHead()
     {
-        size_t i{};
-        Node* current{head};
-        while (current != nullptr)
-        {
-            ++i;
-            current = current->next;
-        }
-
-        delete current;
-        return i;
+        Node* tmp{head};
+        head = head->next;
+        delete tmp;
     }
+
 };
 
 #endif
