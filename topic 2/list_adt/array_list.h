4d6ec0c9837c9e714713b176966c2c8c491fc55b
amortada1 <alimortada05@gmail.com>
2024-03-25 09:47:22 -0700

    added initial list adt files

---
+++ topic 2/list_adt/array_list.h
@@ -0,0 +1,11 @@
+#include <stdexcept>
+
+#include "list.h"
+
+#ifndef ARRAY_LIST
+#define ARRAY_LIST
+
+
+
+#endif /* LINKED_LIST */
+
---
+++ topic 2/list_adt/linked_list.h
@@ -0,0 +1,10 @@
+#include <stdexcept>
+
+#include "list.h"
+
+#ifndef LINKED_LIST
+#define LINKED_LIST
+
+
+
+#endif /* LINKED_LIST */
