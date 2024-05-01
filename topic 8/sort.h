8f1b2c73adddaa4306f2b0e68b4dd99f6018ad23
amortada1 <alimortada05@gmail.com>
2024-05-01 11:29:22 -0700

    implemented radix sort

--- topic 8/sort.h
+++ topic 8/sort.h
@@ -1,14 +1,45 @@
 #include <cstddef>
+#include <cstdint>
 #include <functional>
+#include <vector>
 
 #ifndef SORTER_H
 #define SORTER_H
+
+using Bucket = std::vector<uint32_t>;
 
 template<typename T>
 class Sort
 {
     using Comparator = std::function<bool(const T&, const T&)>;
 public:
+    
+    static void radixSort(uint32_t array[], size_t size)
+    {
+        Bucket buckets[2];
+        buckets[0].reserve(size);
+        buckets[1].reserve(size);
+        const uint32_t MASK{1};
+        size_t i, j, k;
+        size_t v0idx, v1idx;
+        
+        for (i = 0; i < (sizeof(array[0]) << 3); ++i)
+        {
+            k = 0;
+            v0idx = 0;
+            v1idx = 0;
+
+            for (j = 0; j < size; ++j)
+                (((array[j] & (MASK << i)) == 0) ? buckets[0][v0idx++] : buckets[1][v1idx++]) = array[j];
+
+            for (j = 0; j < v0idx; ++j)
+                array[k++] = buckets[0][j];
+
+            for (j = 0; j < v1idx; ++j)
+                array[k++] = buckets[1][j];
+        }
+    }
+    
     static void bubbleSort(T array[], size_t size, Comparator compare)
     {
         bool swapped;
--- topic 8/test.cpp
+++ topic 8/test.cpp
@@ -2,19 +2,21 @@
 #include "test.h"
 
 #define ARRAY_SIZE 8
-#define TEST_ARRAY int array[]{3, 1, 2, 5, 6, 4, 9, 8}
-#define SOLUTION_ARRAY int solutionArray[]{1, 2, 3, 4, 5, 6, 8, 9}
+#define TEST_ARRAY uint32_t array[]{3, 1, 2, 5, 6, 4, 9, 8}
+#define SOLUTION_ARRAY uint32_t solutionArray[]{1, 2, 3, 4, 5, 6, 8, 9}
 
 void testBubbleSort();
 void testSelectionSort();
 void testInsertionSort();
-bool compare(int a, int b) {return a < b;}
+void testRadixSort();
+bool compare(uint32_t a, uint32_t b) {return a < b;}
 
 int main(int argc, char const *argv[])
 {
     // testBubbleSort();
     // testSelectionSort();
-    testInsertionSort();
+    // testInsertionSort();
+    testRadixSort();
     
     return 0;
 }
@@ -22,11 +24,11 @@ int main(int argc, char const *argv[])
 void testBubbleSort()
 {
     TEST_ARRAY;
-    int lastVal;
+    uint32_t lastVal;
 
     size_t size{sizeof(array) / sizeof(array[0])};
 
-    Sort<int>::bubbleSort(array, size, compare);
+    Sort<uint32_t>::bubbleSort(array, size, compare);
     lastVal = array[0];
 
     for (size_t i = 1; i < size; ++i)
@@ -47,11 +49,11 @@ void testBubbleSort()
 void testSelectionSort()
 {
     TEST_ARRAY;
-    int lastVal;
+    uint32_t lastVal;
 
     size_t size{sizeof(array) / sizeof(array[0])};
 
-    Sort<int>::selectionSort(array, size, compare);
+    Sort<uint32_t>::selectionSort(array, size, compare);
     lastVal = array[0];
 
     for (size_t i = 1; i < size; ++i)
@@ -75,7 +77,7 @@ void testInsertionSort()
     TEST_ARRAY;
     SOLUTION_ARRAY;
 
-    Sort<int>::insertionSort(array, ARRAY_SIZE, compare);
+    Sort<uint32_t>::insertionSort(array, ARRAY_SIZE, compare);
 
     for (size_t i = 1; i < ARRAY_SIZE; ++i)
     {   
@@ -89,4 +91,23 @@ void testInsertionSort()
     testPass("insertion sort properly ordered");
 
     return;   
+}
+
+void testRadixSort()
+{
+    TEST_ARRAY;
+    SOLUTION_ARRAY;
+
+    Sort<uint32_t>::radixSort(array, ARRAY_SIZE);
+
+    for (size_t i = 1; i < ARRAY_SIZE; ++i)
+    {   
+        if(array[i] != solutionArray[i])
+        {
+            testFail("radix sort properly ordered");
+            return;
+        }
+    }
+    
+    testPass("radix sort properly ordered");
 }
